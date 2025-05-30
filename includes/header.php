d23980a0bd5f9053d0f29bdd133eefdf38327f23
sukanta saha <sukantasaha481@gmail.com>
2025-05-30 20:11:33 +0530

    header hamburger fixed

--- includes/header.php
+++ includes/header.php
@@ -30,3 +30,10 @@ include_once __DIR__ . '/config.php';
     </div>
   </div>
 </nav>
+
+<!-- ... your page content ... -->
+
+<!-- IMPORTANT: Bootstrap JS for responsive navbar -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
