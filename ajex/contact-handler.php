6a5cc8a58be6ee9ad21700d37fca03fae2c7adc6
sukanta saha <sukantasaha481@gmail.com>
2025-05-30 17:34:55 +0530

    working on contact handler

--- ajex/contact-handler.php
+++ ajex/contact-handler.php
@@ -1,5 +1,6 @@
 <?php
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 header('Content-Type: application/json');
 require_once '../includes/db.php'; // adjust path if needed
