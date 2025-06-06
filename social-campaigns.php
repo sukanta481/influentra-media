1633bb406598bbd586f96879ac29c82bc60b8c74
sukanta481 <53001490+sukanta481@users.noreply.github.com>
2025-06-06 20:22:17 +0530

    Add service pages and link service cards

---
+++ social-campaigns.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+<div class="container py-5">
+  <h1 class="text-center mb-4">Social Campaigns</h1>
+  <p class="lead text-center">Details about our Social Campaigns service will be placed here.</p>
+</div>
+<?php include 'includes/footer.php'; ?>
---
+++ website-development.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+<div class="container py-5">
+  <h1 class="text-center mb-4">Website Development</h1>
+  <p class="lead text-center">Details about our Website Development service will be placed here.</p>
+</div>
+<?php include 'includes/footer.php'; ?>
